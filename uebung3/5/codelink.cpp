bd52d4953ecdb25a6c3b89b9ce82356370adc2bd
Richard Fussenegger (Fleshgrinder) <fleshgrinder@gmx.at>
2012-12-01 16:37:02 +0100

    Übung 3.5 64 bit fix für max()

--- uebung3/5/codelink.cpp
+++ uebung3/5/codelink.cpp
@@ -12,8 +12,8 @@
 extern "C" unsigned calc_expr();
 
 int main() {
-  printf("architecture %s\n", sizeof(void *) == 8 ? "64bit" : "32bit");
-  printf("expr(compiled) = %d\n", calc_expr());
+    printf("architecture %s\n", sizeof (void *) == 8 ? "64bit" : "32bit");
+    printf("expr(compiled) = %d\n", calc_expr());
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
