c7fe9e819a78cac86289631df3698737779a7ab7
Richard Fussenegger (Fleshgrinder) <fleshgrinder@gmx.at>
2013-01-23 15:51:42 +0100

    Finished the cache simulator (exercise 5).

--- uebung5/main.cpp
+++ uebung5/main.cpp
@@ -19,6 +19,7 @@ using namespace std;
  * Print a nice separator on the console.
  */
 void printSeparator() {
+    cout << endl;
     for (int i = 0; i < 80; i++) {
         cout << "-";
     }
@@ -31,18 +32,26 @@ void printSeparator() {
  * @return
  *   Always returns EXIT_SUCCESS.
  */
-int main() {
+int main(int argc, const char* argv[]) {
     // TODO: Maybe we should create a base class CacheSimulator and two child classes. One for the
     // InstructionCacheSimulator (only reading instructions) and one for the DataCacheSimulator (
     // only reading data)?
-    CacheSimulator iCache(4096, direct_mapped, 32);
-    //InstructionCacheSimulator iCache(4096, direct_mapped, 32);
-    CacheSimulator dCache(256 * 1024, full_assoc, 32);
-    //DataCacheSimulator dCache(256 * 1024, full_assoc, 32);
-    dCache->setAssociativityLevel(4);
+    CacheSimulator iCache(4096, DIRECT_MAPPED, 32);
+    //InstructionCacheSimulator iCache(4096, DIRECT_MAPPED, 32);
+
+    printSeparator();
+    cout << "Instruction Cache:" << endl << iCache.getConfiguration();
+
+    CacheSimulator dCache(256 * 1024, FULLY_ASSOCIATIVE, 32);
+    //DataCacheSimulator dCache(256 * 1024, FULLY_ASSOCIATIVE, 32);
+    dCache.setAssociativityLevel(4);
+
+    printSeparator();
+    cout << "Data Cache:" << endl << dCache.getConfiguration();
+    printSeparator();
 
     // Prepare reading of lackey memory trace file.
-    ifstream inputStream("trace-ls.txt");
+    ifstream inputStream(argv[0]);
     string line;
 
     // Read trace line by line.
@@ -51,6 +60,13 @@ int main() {
         if (line.find("I") != string::npos) iCache.read(line);
         else dCache.read(line);
     }
+
+    cout << endl;
+    printSeparator();
+    cout << "Instruction Cache:" << endl << iCache.toString();
+    printSeparator();
+    cout << "Data Cache:" << endl << dCache.toString();
+    printSeparator();
 
     return EXIT_SUCCESS;
 }
